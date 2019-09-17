acdde162807b590916f8bb6ab9d169a6ce7f4ede
Linjiaze123 <1185137488@qq.com>
2019-09-17 22:52:43 +0800

    created a new class

---
+++ Work2.java
@@ -0,0 +1,10 @@
+public class Work2 {
+	public static void main(String[] agrs) {
+		int count = 10;
+		count *= count + 2;
+		count /= 100;
+		count++;
+		
+		System.out.printf("count = %d",count);
+	}
+}
